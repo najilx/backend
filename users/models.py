6799bfe79d166d7135d0d0266318af19e96d12c4
Najil Arfak <najilarfak202@gmail.com>
2025-04-30 15:54:12 +0530

    refactor: removed Contact model from users app and cleaned up related files

--- users/models.py
+++ users/models.py
@@ -15,14 +15,3 @@ class Profile(models.Model):
         return self.user.username
 
 
-# Contact model stores a user’s contact information.
-class Contact(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users_app_contacts')
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    address = models.CharField(max_length=255, blank=True)
-    company = models.CharField(max_length=100, blank=True)
-    phone_numbers = models.CharField(max_length=50, blank=True)
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
